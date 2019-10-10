6d1a156209015cadea6d44e435aac1b059ca548b
huangdian <870003719@qq.com>
2019-10-10 11:23:15 +0800

    1.新增 测试

--- 慕课-数据结构与算法/test/array/lesson1.test.js
+++ 慕课-数据结构与算法/test/array/lesson1.test.js
@@ -1,1 +1,6 @@
 // 17. 电话号码的字母组合
+import telComb from '../../code/array/lesson1'
+
+test('telcomb:23', () => {
+  expect(telComb('23')).toEqual(['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'])
+})
