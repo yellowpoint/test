2685dd84baf86bb2420154c9768f915cc6370cb4
huangdian <870003719@qq.com>
2019-10-10 18:57:34 +0800

    1.优化 文件

--- 慕课-数据结构与算法/code/array/lesson3.js
+++ 慕课-数据结构与算法/code/array/lesson3.js
@@ -5,16 +5,38 @@
  * @return {boolean}
  */
 export default (arr, n) => {
-  let num1 = 0
-  let len = arr.length
-  let maxFlower = len % 2 === 0 ? len / 2 : len / 2 + 1
-  arr.forEach(item => {
-    if (item === 1) {
-      num1++
-    }
-  })
-  if (num1 + n > maxFlower) {
-    return false
+  let tem = []
+  let maxFlower = 0
+  let result = false
+  tem = arr.join('').split('1')
+  if (n === 0) {
+    return true
   }
-  return true
+  console.log('tem', tem)
+  for (let i = 0, len = tem.length, cLength = 0; i < len; i++) {
+    cLength = 0
+    if (len === 1) {
+      cLength = tem[i].length
+    } else {
+      if (i === 0 || i === len - 1) {
+        if (tem[i].length) {
+          cLength = tem[i].slice(1).length
+        }
+      } else {
+        if (tem[i].length > 2) {
+          cLength = tem[i].slice(2).length
+        }
+      }
+      console.log('cLength', i, cLength)
+    }
+
+    let cMax = Math.ceil(cLength / 2)
+    maxFlower += cMax
+    if (n <= maxFlower) {
+      result = true
+      break
+    }
+  }
+
+  return result
 }
