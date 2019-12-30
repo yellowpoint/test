5b52632527a8ff448ffa903185c9bb3bc69322b9
huangdian <870003719@qq.com>
2019-12-30 17:32:35 +0800

    1.删除 掘金小册，开了个新仓库，启动了pages

--- 掘金小册_前端面试之道/nav.js
+++
@@ -1,4 +0,0 @@
-let all = document.querySelectorAll('.section-link');
-[].slice.call(all).forEach((item, index) => {
-  item.setAttribute('href', `./${index+1}-${item.querySelector('.title').innerText}.htm`)
-})
